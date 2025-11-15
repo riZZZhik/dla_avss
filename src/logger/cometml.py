214f299cc70023ed4eefdb95b369fd7a4102fa9e
KovylyaevHSE <sasha_kovylyaev@mail.ru>
2025-11-15 16:01:05 +0300

    Fixed: audio, image logging, cometml config

--- src/logger/cometml.py
+++ src/logger/cometml.py
@@ -177,10 +177,13 @@ class CometMLWriter:
                 in the CometML-friendly format.
         """
         self.exp.log_image(
-            image_data=image, name=self._object_name(image_name), step=self.step
+            image_data=image,
+            name=self._object_name(image_name),
+            step=self.step,
+            image_channels="first",
         )
 
-    def add_audio(self, audio_name, audio, sample_rate=16000):
+    def add_audio(self, audio_name, audio, sample_rate=16000, metadata={}):
         """
         Log an audio to the experiment tracker.
 
@@ -195,6 +198,7 @@ class CometMLWriter:
             audio_data=audio,
             sample_rate=sample_rate,
             step=self.step,
+            metadata=metadata,
         )
 
     def add_text(self, text_name, text):
