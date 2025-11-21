14af06ccbc08f663811a63fcc230af0b46c4092f
olimpiadnik <maxim_afanasev@icloud.com>
2025-11-21 18:26:17 +0000

    New custom dir dataset

---
+++ src/datasets/custom_dir_dataset.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+from src.datasets.base_dataset import BaseDataset
+
+
+class CustomDirAVDataset(BaseDataset):
+
+    def __init__(self, root_dir: str, *args, **kwargs):
+        root_dir = Path(root_dir)
+        audio_root = root_dir / "audio"
+        mix_dir = audio_root / "mix"
+        s1_dir = audio_root / "s1"
+        s2_dir = audio_root / "s2"
+        mouths_root = root_dir / "mouths"
+
+        exts = {".wav", ".flac", ".mp3", ".m4a"}
+
+        data = []
+        for mix_path in mix_dir.iterdir():
+            if mix_path.suffix.lower() not in exts:
+                continue
+
+            mix_name = mix_path.name
+            stem = mix_path.stem  
+
+            if "_" in stem:
+                s1_id, s2_id = stem.split("_", 1)
+            else:
+                s1_id = stem
+                s2_id = stem
+
+            row = {
+                "audio_path": str(mix_path),
+            }
+
+            s1_audio = s1_dir / mix_name
+            s2_audio = s2_dir / mix_name
+
+            if s1_audio.exists() and s2_audio.exists():
+                row["speaker1_audio_path"] = str(s1_audio)
+                row["speaker2_audio_path"] = str(s2_audio)
+
+            s1_video = mouths_root / f"{s1_id}.npz"
+            s2_video = mouths_root / f"{s2_id}.npz"
+
+            if s1_video.exists():
+                row["speaker1_video_path"] = str(s1_video)
+            if s2_video.exists():
+                row["speaker2_video_path"] = str(s2_video)
+
+            data.append(row)
+
+        super().__init__(data, *args, **kwargs)
