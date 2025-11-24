0e53e46d323708b7eabdff6d34112de1c7208cbe
KovylyaevHSE <sasha_kovylyaev@mail.ru>
2025-11-24 16:08:55 +0300

    Fix: metric si_snri

--- src/metrics/si_snri.py
+++ src/metrics/si_snri.py
@@ -20,24 +20,16 @@ class SI_SNRiMetric(BaseMetric):
         speaker2 = speakers[..., 1, :]
 
         val_per1 = (
-            (
-                calc_si_snr(preds_s1, speaker1)
-                - calc_si_snr(mix, speaker1)
-                + calc_si_snr(preds_s2, speaker2)
-                - calc_si_snr(mix, speaker2)
-            ).sum()
-            / 2
-            / preds.shape[0]
-        )
+            calc_si_snr(preds_s1, speaker1)
+            - calc_si_snr(mix, speaker1)
+            + calc_si_snr(preds_s2, speaker2)
+            - calc_si_snr(mix, speaker2)
+        ).mean()
         val_per2 = (
-            (
-                calc_si_snr(preds_s1, speaker2)
-                - calc_si_snr(mix, speaker2)
-                + calc_si_snr(preds_s2, speaker1)
-                - calc_si_snr(mix, speaker1)
-            ).sum()
-            / 2
-            / preds.shape[0]
-        )
+            calc_si_snr(preds_s1, speaker2)
+            - calc_si_snr(mix, speaker2)
+            + calc_si_snr(preds_s2, speaker1)
+            - calc_si_snr(mix, speaker1)
+        ).mean()
 
         return max(val_per1, val_per2)
